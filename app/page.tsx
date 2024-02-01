500704019443de091a1f751c1b80870b2b8b24be
Daniel Mateu <danielmateu86@gmail.com>
2024-02-01 12:29:52 +0100

    adding shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
-
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <h1 className="font-semibold text-center">Hello auth!</h1>
+    <>
+      <h1 className="font-semibold text-center">Hello auth!</h1>
+      <Button size="lg">Button</Button>
+    </>
   );
 }
